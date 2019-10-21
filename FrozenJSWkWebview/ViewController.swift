cbb1f8adec93e8f4b9af8c486ce2bf4d9cff92d6
André Herculano <andresilveirah@gmail.com>
2019-10-21 11:36:58 +0200

    Initial Commit

---
+++ FrozenJSWkWebview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FrozenJSWkWebview
+//
+//  Created by Andre Herculano on 21.10.19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
